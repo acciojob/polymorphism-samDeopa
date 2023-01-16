fb781b7d0003bd166e9611b368a26ecd9eeb2d51
samdeopa <you@example.com>
2023-01-16 12:45:16 +0530

    completed the task

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,44 @@
 package com.driver;
+/*
 
+Task 1:
+create a class Product inside Main class
+Task 2:
+create object of Product in Main function called p
+Task 3:
+create a method of following defination
+public int product(int x, int y) {}
+call this method from Main using Product class object p
+Task 4:
+create a Overloaded method product of following defination
+public int product(int x, int y, int z) {}
+call this method also from Main using Product class object p
+Task 5:
+create a Overloaded method product of following defination
+public double product(double x, double y) {}
+call this method also from Main using Product class object p
+Observations:
+This class contains 3 classes with same name, but it complie & run successfully
+*/
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1,2);
+
+    }
+}
+class  Product{
+    public int product(int x, int y){
+        return  x+y;
+    }
+    public int product(int x, int y, int z) {
+        return  x+y+z;
+    }
+    public double product(double x, double y) {
+        return x+y;
+    }
+
 
 }
